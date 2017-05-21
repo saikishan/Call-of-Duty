9ffe3ac6a33e44b589a423001f1d93fbf0670d80
saikishan <saikishan2008@gmail.com>
2017-05-22 02:18:46 +0530

    just few modifications to adjust the width

--- trooper.js
+++ trooper.js
@@ -5,9 +5,14 @@ var Trooper = function(x,y){
 Trooper.prototype.setup = function(){
    var b = $("<img class='trooper enemy' src='images/trooper.svg'>").css({"left":this.xpos+"px","top":this.ypos+"px"})
    b.load(function(){
-        $(this).animate({'top':"95%"},4000,"swing",function(){
-            console.log("reached grond")
-        })
+        $(this).animate({'top':"50%"},4000,"linear",function(){
+            $(this).attr('src',"images/paratrooper.svg");
+            $(this).css('width','3%');
+            $(this).animate({'top':"90%"},5000,"linear",function(){
+                $(this).css('width','0.8%');
+              $(this).attr('src',"images/trooper.svg");
+            });
+        });
     });
     $('body').append(b);
 }
